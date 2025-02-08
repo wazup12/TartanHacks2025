5c2ee4bad5c91bd388deef83e76ec3a75eefd44f
Julius <julius.arolovitch@gmail.com>
2025-02-08 03:35:45 -0500

    gradients

--- depth_estimator.py
+++ depth_estimator.py
@@ -1,42 +1,32 @@
 import cv2
 import torch
-import urllib.request
-
 import matplotlib.pyplot as plt
+import numpy as np
 
-url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
-urllib.request.urlretrieve(url, filename)
+filename = "img.jpeg"  
 
-
-model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
-#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
-#model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+model_type = "DPT_Hybrid"  
 
 midas = torch.hub.load("intel-isl/MiDaS", model_type)
-
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 midas.to(device)
 midas.eval()
 
-
 midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 
-if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+if model_type in ["DPT_Large", "DPT_Hybrid"]:
     transform = midas_transforms.dpt_transform
 else:
     transform = midas_transforms.small_transform
 
-
 img = cv2.imread(filename)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
 
 input_batch = transform(img).to(device)
 
-
 with torch.no_grad():
     prediction = midas(input_batch)
-
     prediction = torch.nn.functional.interpolate(
         prediction.unsqueeze(1),
         size=img.shape[:2],
@@ -45,3 +35,13 @@ with torch.no_grad():
     ).squeeze()
 
 output = prediction.cpu().numpy()
+
+grad_x, grad_y = np.gradient(output)
+
+gradient_magnitude = np.sqrt(grad_x**2 + grad_y**2)
+
+incline_angle = np.degrees(np.arctan(gradient_magnitude))
+
+np.save("gradient.npy", incline_angle)
+
+print("Incline angle map saved as 'gradient.npy'")
